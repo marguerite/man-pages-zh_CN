34be39196b409d3fbe528622dde7cfdff845aef8
marguerite <i@marguerite.su>
2014-07-31 01:12:57 +0800

    clean started

---
+++ release.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+VERSION=`cat manpages-zh/VERSION | sed 's/v//'`
+echo $VERSION
+cp -r manpages-zh man-pages-zh_CN-${VERSION}
+# tarball
+tar -czf man-pages-zh_CN-${VERSION}.tar.gz man-pages-zh_CN-${VERSION}
+# xz
+tar -cf man-pages-zh_CN-${VERSION}.tar man-pages-zh_CN-${VERSION}
+xz -z -9 man-pages-zh_CN-${VERSION}.tar
+echo "successfully released ${VERSION}!"
+rm -rf man-pages-zh_CN-${VERSION}
